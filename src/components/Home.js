b5aea956f48efe710ce8e54a6558b45527388cbe
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-01-04 12:45:23 -0800

    created user registration to communicate and create user instance ointo the backend

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Registration from './auth/Registration';
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <h1>Home</h1>
+                <Registration />
+            </div>
+        </div>
+        
+    );
+}
+
+export default Home;
